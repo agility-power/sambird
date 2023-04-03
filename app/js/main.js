98ea2fd7fdfe15fd77b6b689b9a3860556ca9ba9
agility-power <4gilitypower@gmail.com>
2023-04-03 16:55:55 +0300

    slick slider settings

--- app/js/main.js
+++ app/js/main.js
@@ -11,21 +11,13 @@ $(function () {
     $('.slider__items').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
+        centerMode: true,
+        variableWidth: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
         arrows: false,
         dots: true,
         fade: true,
     })
-
-
-
-
-    //   $('.product-tabs__top-item').on('click', function (e) {
-    //     e.preventDefault();
-    //     $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
-    //     $(this).addClass('product-tabs__top-item--active');
-
-    //     $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
-    //     $($(this).attr('href')).addClass('product-tabs__content-item--active');
-    //   })
 
 })
