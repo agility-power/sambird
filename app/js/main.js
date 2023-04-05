35897bf117a72cfffb314eb4c1f19002aeb2e2af
agility-power <4gilitypower@gmail.com>
2023-04-05 21:39:19 +0300

    add mobile burger menu and header

--- app/js/main.js
+++ app/js/main.js
@@ -21,3 +21,20 @@ $(function () {
     })
 
 })
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+let menuItem = document.querySelectorAll('.menu__item');
+
+menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('menu-btn--active');
+    menu.classList.toggle('menu--active');
+})
+
+
+menuItem.forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+        menuBtn.classList.toggle('menu-btn--active');
+        menu.classList.toggle('menu--active');
+    })
+})
